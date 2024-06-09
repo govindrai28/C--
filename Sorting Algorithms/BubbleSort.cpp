a3e0dc308ec22f373e23f27e92fabe3fa83a5169
Govind Rai <raigovind8770@gmail.com>
2024-06-09 11:29:17 +0530

    swapped variable is chaged if already sorted then it will break the for loop and exit

--- Sorting Algorithms/BubbleSort.cpp
+++ Sorting Algorithms/BubbleSort.cpp
@@ -4,13 +4,19 @@ int main()
 {
     int arr[]={10,3,4,2,9,5,7};
     int n = sizeof(arr)/sizeof(arr[0]);
+    bool swapped;
     for(int i=0;i<n-1;i++)
     {
-        for(int j=0;j<n-i-1;j++)
+        swapped = 0;
+        for(int j=0;j<n-1-i;j++)
         {
             if(arr[j]>arr[j+1])
+            {
                 swap(arr[j],arr[j+1]);
+                swapped = 1;
+            }  
         }
+        if(swapped == 0) break;
     }
     for(int i=0;i<n;i++)
     {
