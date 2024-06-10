8f7625fe2976b596c759813f027ac659db72a5a9
Govind Rai <raigovind8770@gmail.com>
2024-06-10 16:23:36 +0530

    Insertion Sort

--- Sorting Algorithms/InsertionSort.cpp
+++ Sorting Algorithms/InsertionSort.cpp
@@ -1,10 +1,35 @@
 #include<iostream>
 using namespace std;
+void InsertionSort(int arr[],int n)
+{
+    for(int i=1;i<n;i++)//0th element is already sorted
+    {
+        int j = i-1; // for comparision 
+        int temp = arr[i]; // copy that value
+        while(j>=0 && arr[j]>temp)
+        {
+            arr[j+1]=arr[j];//for shifthing
+            j--;
+        }
+        arr[j+1]= temp;
+
+    }
+
+}
+void printArray(int arr[],int n)
+{
+    for(int i = 0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
 int main()
 {
-    int sbi;
-    cout<<"my name is Govind Rai";
-    cout<<"final";
-    cout<<"nice you perfectly learned that one";
+    int arr[] = {5,4,3,2,1};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    InsertionSort(arr,n);
+    printArray(arr,n);
+    
+    
     return 0;
 }
